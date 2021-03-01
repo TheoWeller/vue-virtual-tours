ae84a63b04eeb45ed339e18ab6a42a978a2aa27a
Theo Weller <weller.theodor@gmail.com>
2021-03-01 12:56:42 -0600

    initial commit

---
+++ src/components/MyForm.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <input type="text" placeholder="name" />
+    </div>
+    <div>
+        <button>Submit</button>
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+    setup(){
+        const name = ref('')
+
+        return {
+            name,
+        }
+    },
+}
+</script>
+
+<style>
+input[type="text"]{
+    padding: 5px;
+}
+
+button {
+    margin-top: 20px;
+}
+</style>
